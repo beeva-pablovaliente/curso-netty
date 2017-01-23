c58f69c4ecc01ebb7dd70595d51c6c1a65c7731e
Pablo Valiente <pabloantonio.valiente@beeva.com>
2017-01-23 23:07:28 +0100

    Added ByteBuf Server implementation

--- src/main/java/com/beeva/formacion/netty/handler/ByteBufServerHandler.java
+++ src/main/java/com/beeva/formacion/netty/handler/ByteBufServerHandler.java
@@ -1,7 +1,21 @@
 package com.beeva.formacion.netty.handler;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+import java.nio.charset.Charset;
+
 /**
  * Created by Architecture Team
  */
-public class ByteBufServerHandler {
+public class ByteBufServerHandler extends SimpleChannelInboundHandler<ByteBuf> {
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, ByteBuf msg) throws Exception {
+        System.out.println(msg.toString(Charset.forName("UTF-8")));
+
+        ctx.writeAndFlush(msg.alloc()).addListener(ChannelFutureListener.CLOSE);
+    }
 }
--- src/main/java/com/beeva/formacion/netty/server/Server.java
+++ src/main/java/com/beeva/formacion/netty/server/Server.java
@@ -1,7 +1,51 @@
 package com.beeva.formacion.netty.server;
 
+import com.beeva.formacion.netty.handler.ByteBufServerHandler;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
 /**
  * Created by Beeva Architecture Team
  */
 public class Server {
+
+    public static void main(String[] args) throws InterruptedException{
+
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup(Runtime.getRuntime().availableProcessors());
+
+        ServerBootstrap b = new ServerBootstrap();
+        b.group(bossGroup, workerGroup)
+                .channel(NioServerSocketChannel.class)
+                .localAddress(8080)
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel ch) throws Exception {
+                        ch.pipeline()
+                                .addLast(new ByteBufServerHandler());
+                    }
+                });
+
+        try {
+            ChannelFuture cf = b.bind().sync();
+            cf.addListener(future -> {
+                if (future.isSuccess()) {
+                    System.out.println("Server Up");
+                }
+            });
+
+            cf.channel().closeFuture().sync();
+        }
+        finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+
+    }
+
 }
